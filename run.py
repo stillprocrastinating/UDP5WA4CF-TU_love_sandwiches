84fd328c713913b7e613cbeb62cae96da23f2930
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-11-23 22:21:27 +0000

    validate_data() > try > [int(value) for value in values]

--- run.py
+++ run.py
@@ -32,8 +32,11 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into integers, or if there aren't exaxtly 6 values.
     """
     try:
+        [int(value) for value in values]
+
         if len(values) != 6:
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+        
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
